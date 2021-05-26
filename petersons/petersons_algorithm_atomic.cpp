e49b34b20f423f212b43f30af730c9b0bbb724be
eg82 <Yogendra.Prabhu@tesco.com>
2021-05-26 19:38:02 +0530

    Mem reordering and atomics

---
+++ petersons/petersons_algorithm_atomic.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <assert.h>
+#include <atomic>
+
+struct PetersonsAlgorithm
+{
+    void entry(int thread_id)
+    {
+        int otherThreadId = 1 ^ thread_id; // xor is 1 if 0 and 0 if 1
+        turn = otherThreadId;
+        flag[thread_id] = true;
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        while (flag[otherThreadId] && turn == otherThreadId)
+            ;
+    }
+
+    void exit(int thread_id)
+    {
+        flag[thread_id] = false;
+    }
+
+private:
+    std::atomic<int> turn{0};
+    std::atomic<bool> flag[2];
+};
+
+void total_sum(PetersonsAlgorithm &petersons_algorithm, int &counter, int thread_id)
+{
+    for (int i = 0; i < 1000000; i++)
+    {
+        petersons_algorithm.entry(thread_id);
+        counter++;
+        petersons_algorithm.exit(thread_id);
+    }
+}
+
+int main()
+{
+    PetersonsAlgorithm petersons_algorithm;
+    int counter = 0;
+
+    std::thread thread0(total_sum, std::ref(petersons_algorithm), std::ref(counter), 0);
+    std::thread thread1(total_sum, std::ref(petersons_algorithm), std::ref(counter), 1);
+
+    thread0.join();
+    thread1.join();
+
+    std::cout << "Counter value is : " << counter << std::endl;
+    assert(counter == 2000000);
+}
---
+++ petersons/petersons_algorithm_mem_reordering.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <thread>
+#include <assert.h>
+
+struct PetersonsAlgorithm
+{
+  void entry(int thread_id)
+  {
+    flag[thread_id] = true;
+    int otherThreadId = 1 ^ thread_id; // xor is 1 if 0 and 0 if 1
+    turn = otherThreadId;
+    //Possible reordering
+    while (flag[otherThreadId] && turn == otherThreadId)
+      ;
+  }
+
+  void exit(int thread_id)
+  {
+    flag[thread_id] = false;
+  }
+
+private:
+  int turn = 0;
+  bool flag[2];
+};
+
+void total_sum(PetersonsAlgorithm &petersons_algorithm, int &counter, int thread_id)
+{
+  for (int i = 0; i < 1000000; i++)
+  {
+    petersons_algorithm.entry(thread_id);
+    counter++;
+    petersons_algorithm.exit(thread_id);
+  }
+}
+
+int main()
+{
+  PetersonsAlgorithm petersons_algorithm;
+  int counter = 0;
+
+  std::thread thread0(total_sum, std::ref(petersons_algorithm), std::ref(counter), 0);
+  std::thread thread1(total_sum, std::ref(petersons_algorithm), std::ref(counter), 1);
+
+  thread0.join();
+  thread1.join();
+
+  std::cout << "Counter value is : " << counter << std::endl;
+  assert(counter == 2000000);
+}
